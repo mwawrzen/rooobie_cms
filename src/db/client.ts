27aaddfad7a9b3d49a33eb77144b72cf78617805
mwawrzen <w.w@op.pl>
2025-11-26 19:40:27 +0100

    feat: update ts path aliases

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/bun-sqlite";
 import { Database } from "bun:sqlite";
-import * as schema from "@db/schema";
+import * as schema from "@schema";
 
 const sqlite= new Database( `./${ process.env.DB_FILE_NAME! }` );
 export const db= drizzle( sqlite, { schema });
