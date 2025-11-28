a30b5453e178dd4d04397abe4953ec0ed32c0526
mwawrzen <w.w@op.pl>
2025-11-28 11:10:33 +0100

    feat: apply role guard

--- src/api/v1.ts
+++ src/api/v1.ts
@@ -1,5 +1,6 @@
 import { Elysia } from "elysia";
-import { authGuard } from "@auth/middleware"
+import { authGuard } from "@auth/guard.middleware"
+import { requiresAdmin } from "@auth/role.middleware";
 import { authRouter } from "@v1/auth";
 import { adminRouter } from "@v1/admin";
 
@@ -7,5 +8,6 @@ export const apiV1= new Elysia({ prefix: "/api/v1" })
   .use( authRouter )
   .group( "/admin", app=> app
     .use( authGuard )
+    .use( requiresAdmin )
     .use( adminRouter )
   )
