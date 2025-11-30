d01fea31950db506202062c09f0008c1c7e78835
mwawrzen <w.w@op.pl>
2025-11-30 23:03:06 +0100

    feat: update custom errors

--- src/modules/user/errors.ts
+++ src/modules/user/errors.ts
@@ -1,5 +1,7 @@
 export class UserUnauthorizedError extends Error {
+
   status= 401
+
   constructor() {
     super( "Unauthorized" );
     this.name= "UserUnauthorizedError";
@@ -16,15 +18,58 @@ export class UserUnauthorizedError extends Error {
 };
 
 export class UserExistsError extends Error {
-  constructor() {
-    super( `User with given email already exists` );
+
+  status= 409;
+
+  constructor( email: string ) {
+    super( `User with email ${ email } already exists` );
     this.name= "UserExistsError";
+  }
+
+  toResponse() {
+    return Response.json({
+      message: this.message,
+      code: this.status
+    }, {
+      status: this.status
+    });
   }
 };
 
 export class UserNotFoundError extends Error {
-  constructor() {
-    super( "User not found" );
+
+  status= 404;
+
+  constructor( id: number ) {
+    super( `User with id ${ id } not found` );
     this.name= "UserNotFoundError";
   }
+
+  toResponse() {
+    return Response.json({
+      message: this.message,
+      code: this.status
+    }, {
+      status: this.status
+    });
+  }
 };
+
+export class AccessDeniedError extends Error {
+
+  status= 403;
+
+  constructor() {
+    super( "Admin cannot delete their own" );
+    this.name= "AccessDeniedError"
+  }
+
+  toResponse() {
+    return Response.json({
+      message: this.message,
+      code: this.status
+    }, {
+      status: this.status
+    });
+  }
+}
