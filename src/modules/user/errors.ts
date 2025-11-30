62c2f493497e0724b977355d15ece7a5009d4a4e
mwawrzen <w.w@op.pl>
2025-11-30 20:08:18 +0100

    feat: create custom unathorized user error

--- src/modules/user/errors.ts
+++ src/modules/user/errors.ts
@@ -1,3 +1,20 @@
+export class UserUnauthorizedError extends Error {
+  status= 401
+  constructor() {
+    super( "Unauthorized" );
+    this.name= "UserUnauthorizedError";
+  }
+
+  toResponse() {
+    return Response.json({
+      message: this.message,
+      code: this.status
+    }, {
+      status: this.status
+    });
+  }
+};
+
 export class UserExistsError extends Error {
   constructor() {
     super( `User with given email already exists` );
