b8625b6cb7329c6301a916195c0e036ccb06ce77
mwawrzen <w.w@op.pl>
2025-11-30 23:01:05 +0100

    feat: create function removing password from user object

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,13 @@
+import { UserPublic, UserWithPassword } from "../modules/user/schemas";
+
 export const createEnumObject= ( arr: readonly string[] )=> {
   return arr.reduce(( acc, curr )=> {
     acc[ curr ]= curr;
     return acc;
   }, {} as Record<string, string> );
+};
+
+export const removePasswordFromUser= ( user: UserWithPassword ): UserPublic=> {
+  const { passwordHash: _, ...safeUser }= user;
+  return safeUser;
 };
