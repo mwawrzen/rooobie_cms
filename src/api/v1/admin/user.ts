feddcda7ebeba6e1cb252b68a15f3eb3802a2c84
mwawrzen <w.w@op.pl>
2025-11-30 23:01:34 +0100

    feat: relocate business logic to service

--- src/api/v1/admin/user.ts
+++ src/api/v1/admin/user.ts
@@ -1,73 +1,30 @@
-import { IdParamSchema } from "@/src/modules/project/schemas";
-import { UserExistsError, UserNotFoundError } from "@/src/modules/user/errors";
-import { deleteUser, getSafeUserById, getUsers } from "@/src/modules/user/repository";
-import { LoginBodySchema } from "@/src/modules/user/schemas";
-import { registerNewUser } from "@/src/modules/user/service";
 import Elysia from "elysia";
+import { CreateUserBodySchema, IdParamSchema } from "@modules/user/schemas";
+import { userService } from "@modules/user/service";
+import { AccessDeniedError } from "@/src/modules/user/errors";
 
 export const userRouter= new Elysia({ prefix: "/user" })
-  .get( "/", async ({ set })=> {
-    try {
-      const users= await getUsers();
-      return { users };
-    } catch( error ) {
-      set.status= 500;
-      return { error: "Failed to retrieve user list" };
-    }
+  .post( "/", async ({ body, status })=> {
+    const user= await userService.create( body );
+    return status( 201, user );
+  }, {
+    body: CreateUserBodySchema
   })
-  .get( "/:id", async ({ params })=> {
-    const user= await getSafeUserById( params.id );
-
-    if( !user )
-      throw new UserNotFoundError();
-
-    return { user };
+  .get( "/", async ()=> {
+      return await userService.getAll();
+  })
+  .get( "/:id", async ({ params: { id }})=> {
+    return await userService.getById( id );
   }, {
     params: IdParamSchema
   })
-  .post( "/", async ({ body, set })=> {
-    try {
-      const { id, email, createdAt }= await registerNewUser(
-        body.email,
-        body.password
-      );
+  .delete( "/:id", async ({ params: { id }, status, user }: any )=> {
 
-      set.status= 201;
+    if( user&& user.id=== id )
+      throw new AccessDeniedError();
 
-      return {
-        message: "User created successfully",
-        user: { id, email, createdAt }
-      };
-
-    } catch( error ) {
-
-      if( error instanceof UserExistsError ) {
-        set.status= 409;
-        return {
-          error: "Resource confilct",
-          message: error.message
-        };
-      }
-
-      set.status= 500;
-      return { error: "Internal server error" };
-    }
-  }, {
-    body: LoginBodySchema
-  })
-  .delete( "/:id", async ({ params, set, user }: any)=> {
-    if( user&& user.id=== params.id ) {
-      set.status= 403;
-      return { error: "Admin cannot delete their own account via this endpoint" };
-    }
-
-    const deletedCount= await deleteUser( params.id );
-
-    if( deletedCount=== 0 )
-      throw new UserNotFoundError();
-
-    set.status= 200;
-    return { message: "User successfully deleted" };
+    await userService.remove( id );
+    return status( 204 );
   }, {
     params: IdParamSchema
   });
