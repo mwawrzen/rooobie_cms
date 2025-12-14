47e7ef1240ac18b1087e384dcf40012989925dce
mwawrzen <w.w@op.pl>
2025-12-14 20:03:49 +0100

    feat: create content public service

--- src/modules/content/public.service.ts
+++ src/modules/content/public.service.ts
@@ -1,3 +1,16 @@
-export const contentPublicService = {
+import { contentRepository } from "@modules/content/repository";
+import { ContentVariable } from "@modules/content/schemas";
 
+/**
+ * Returns all content variables
+ * @param id Project id
+ */
+async function getAll(
+  id: number,
+): Promise<ContentVariable[]> {
+  return contentRepository.fetchByProjectId( id );
+}
+
+export const contentPublicService = {
+  getAll
 };
